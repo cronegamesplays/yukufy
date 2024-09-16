ae85ea372d3b1f0d05fb18816a00b7aee4fcd80e
Crone Games Plays <77684277+cronegamesplays@users.noreply.github.com>
2024-09-16 17:27:03 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const DistifyClient = require('./distifyClient');
-module.exports = { DistifyClient };
-require("./update");
-//require("./test");
-
-//© 2024 Distify Player Music - Kandaraku Studios | Owner: cronegamesplays
