fe4b6449297c23f1712d254de8e0ef96e4252c17
Crone Games Plays <77684277+cronegamesplays@users.noreply.github.com>
2024-09-16 19:46:20 -0300

    Update Update.js

--- src/functions/Update.js
+++ src/functions/Update.js
@@ -4,11 +4,11 @@ const packageVersion = require("../../package.json").version;
 
 async function checkForUpdates() {
   try {
-    const response = await axios.get("https://registry.npmjs.com/distify.js");
+    const response = await axios.get("https://registry.npmjs.com/yukufy");
     const latestVersion = response.data["dist-tags"].latest;
 
     if (semver.gt(latestVersion, packageVersion)) {
-      console.log(`\x1b[38;5;215mYou are using an outdated version of Distify.js. Update to the latest version using the command: \x1b[38;5;119m'npm i distify.js@${latestVersion}'\x1b[38;5;215m to get new features and bug fixes.\x1B[0m`);
+      console.log(`\x1b[38;5;215mYou are using an outdated version of Yukufy. Update to the latest version using the command: \x1b[38;5;119m'npm i yukufy@${latestVersion}'\x1b[38;5;215m to get new features and bug fixes.\x1B[0m`);
     }
   } catch (err) {
     console.error("Error checking for updates:", err);
